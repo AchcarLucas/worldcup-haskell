82cc9715da9304c2bdcf83f693a4cff22f8a8dc1
Lucas Campos <achcarlucas@gmail.com>
2018-05-19 15:09:25 -0300

    Route to Create User

---
+++ yesod/src/Handler/User.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+module Handler.Home where
+
+import Import
+
+-- This is a handler function for the GET request method on the HomeR
+-- resource pattern. All of your resource patterns are defined in
+-- config/routes
+--
+-- The majority of the code you will write in Yesod lives in these handler
+-- functions. You can spread them across multiple files if you are so
+-- inclined, or create a single monolithic file.
+postCreateUserR :: Handler Value
+postCreateUserR = do
+	user <- requireJsonBody :: Handler User
+	uid <- runDB $ insert user
+	sendStatusJSON created201 (object ["resp" .= fromSqlKey uid])
