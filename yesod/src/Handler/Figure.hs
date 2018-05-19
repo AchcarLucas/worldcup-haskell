33fe1f934c2ea50c76d178470291314b886f8d13
Lucas Campos <achcarlucas@gmail.com>
2018-05-19 14:48:59 -0300

    Route para pegar todas as figurinhas

---
+++ yesod/src/Handler/Figure.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+module Handler.Figure where
+
+import Import
+
+-- This is a handler function for the GET request method on the HomeR
+-- resource pattern. All of your resource patterns are defined in
+-- config/routes
+--
+-- The majority of the code you will write in Yesod lives in these handler
+-- functions. You can spread them across multiple files if you are so
+-- inclined, or create a single monolithic file.
+getAllFigureR :: Handler Value
+getAllFigureR = do
+	all_figure <- runDB $ selectList [] [Asc FigureFigure_id]
+	sendStatusJSON ok200 (object ["resp" .= all_figure])
