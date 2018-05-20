949e66ce31bd4810cb10d0fe256a3a7222bf7724
Lucas Campos <achcarlucas@gmail.com>
2018-05-20 19:35:38 -0300

    onLogin na tela de login - parcial

--- ionic-app/worldcup-app/src/pages/login/login.ts
+++ ionic-app/worldcup-app/src/pages/login/login.ts
@@ -4,6 +4,7 @@ import { Validators, FormBuilder } from '@angular/forms';
 import { Storage } from '@ionic/storage';
 
 import { GlobalProvider } from '../../providers/global/global';
+import { LoginProvider } from '../../providers/login/login';
 
 import { HomePage } from '../../pages/home/home';
 import { RegisterPage } from '../../pages/register/register';
@@ -27,7 +28,9 @@ export class LoginPage {
 	constructor(
 			public navCtrl: NavController, 
 			public navParams: NavParams,
-			public formBuilder : FormBuilder, ) {
+			public formBuilder: FormBuilder, 
+			public globalProvider: GlobalProvider, 
+			public loginProvider: LoginProvider) {
 		this.login = this.formBuilder.group({
 			email : ['', Validators.email],
 			password : ['', Validators.required],
@@ -40,7 +43,20 @@ export class LoginPage {
 	}
 
 	onLogin() {
-		this.navCtrl.setRoot(HomePage);
+		let data = {email:this.login.value.email, password:this.login.value.password};
+		this.loginProvider.onLogin(data).subscribe(
+			success=>this.onSuccessLogin(success),
+			error=>this.onErrorLogin(error)
+		);
+		//this.navCtrl.setRoot(HomePage);
+	}
+
+	onSuccessLogin(success) {
+		console.log(success);
+	}
+
+	onErrorLogin(error) {
+		console.log(error);
 	}
 
 	onRegister() {
