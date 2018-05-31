3ed956983e36fb5bb8a30807eebb9588a0daa861
Lucas Campos <achcarlucas@gmail.com>
2018-05-31 17:59:25 -0300

    Filtro de usuário por figurinha

--- yesod/src/Handler/Search.hs
+++ yesod/src/Handler/Search.hs
@@ -26,7 +26,15 @@ getSearchUserGpsR page s_lgt s_lng = do
 	let resultsPerPage = 15
 	lgt <- convertDouble s_lgt
 	lng <- convertDouble s_lng
-	clients <- selectUserByDistance resultsPerPage page lgt lng
+	clients <- selectUsersByDistance resultsPerPage page lgt lng
+	sendStatusJSON ok200 (object ["resp" .= clients])
+
+-- Recupera o Usuário pela Figurinha
+getSearchUserFigureR :: Int -> String -> String -> Handler Value
+getSearchUserFigureR fid s_lgt s_lng = do
+	lgt <- convertDouble s_lgt
+	lng <- convertDouble s_lng
+	clients <- selectUsersByFigure fid lgt lng
 	sendStatusJSON ok200 (object ["resp" .= clients])
 
 -- Recupera as informações do usuário especifico
@@ -41,7 +49,11 @@ convertDouble :: String -> Handler Double
 convertDouble s = do
 	return $ (read s)::Handler Double
 
--- Seleciona os usuários pela distância
-selectUserByDistance :: Int -> Int -> Double -> Double -> Handler [Entity User]
-selectUserByDistance limit page lgt lng = do 
-	runDB $ rawSql "SELECT ?? FROM public.user ORDER BY (12742 * asin(sqrt(0.5 - cos ((gps_latitude - ?) * 0.017453292519943295) / 2 + cos (? * 0.017453292519943295) * cos(gps_latitude * 0.017453292519943295) * (1 - cos((gps_longitude - ?) * 0.017453292519943295)) / 2))) ASC LIMIT ? OFFSET ?" [toPersistValue lgt, toPersistValue lgt, toPersistValue lng, toPersistValue limit, toPersistValue $ limit*page]
+-- Seleciona os Usuários pela Distância
+selectUsersByDistance :: Int -> Int -> Double -> Double -> Handler [Entity User]
+selectUsersByDistance limit page lgt lng = do 
+	runDB $ rawSql "SELECT ?? FROM public.user WHERE (SELECT COUNT(*) FROM public.figure_user WHERE user_id = public.user.id) > 0 ORDER BY (12742 * asin(sqrt(0.5 - cos ((gps_latitude - ?) * 0.017453292519943295) / 2 + cos (? * 0.017453292519943295) * cos(gps_latitude * 0.017453292519943295) * (1 - cos((gps_longitude - ?) * 0.017453292519943295)) / 2))) ASC LIMIT ? OFFSET ?" [toPersistValue lgt, toPersistValue lgt, toPersistValue lng, toPersistValue limit, toPersistValue $ limit*page]
+
+selectUsersByFigure :: Int -> Double -> Double -> Handler [Entity User]
+selectUsersByFigure fid lgt lng = do 
+	runDB $ rawSql "SELECT ?? FROM public.user WHERE (SELECT COUNT(*) FROM public.figure_user WHERE user_id = public.user.id AND figure_id = ?) > 0 ORDER BY (12742 * asin(sqrt(0.5 - cos ((gps_latitude - ?) * 0.017453292519943295) / 2 + cos (? * 0.017453292519943295) * cos(gps_latitude * 0.017453292519943295) * (1 - cos((gps_longitude - ?) * 0.017453292519943295)) / 2))) ASC" [toPersistValue fid, toPersistValue lgt, toPersistValue lgt, toPersistValue lng]
