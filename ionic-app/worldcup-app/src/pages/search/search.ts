b0fffdd61ea1b74a58d89b39087e2741abfe7e55
Lucas Campos <achcarlucas@gmail.com>
2018-05-31 22:17:18 -0300

    search recebendo informações do usuário

--- ionic-app/worldcup-app/src/pages/search/search.ts
+++ ionic-app/worldcup-app/src/pages/search/search.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 
+import { Storage } from '@ionic/storage';
+
 import { GlobalProvider } from '../../providers/global/global';
 import { SearchProvider } from '../../providers/search/search';
+
+import { LoginPage } from '../../pages/login/login';
+
+import { Geolocation } from '@ionic-native/geolocation';
 
 /**
  * Generated class for the SearchPage page.
@@ -18,14 +24,54 @@ import { SearchProvider } from '../../providers/search/search';
 })
 export class SearchPage {
 
+	logged : any;
 	loading : any;
 
-	constructor(	public navCtrl: NavController, 
-				public navParams: NavParams,
-				public geolocation: Geolocation, 
-				public globalProvider: GlobalProvider, 
-				public loadingCtrl: LoadingController,
-				public storage: Storage,) {
+	page : number = 0;
+
+	constructor(	public navCtrl: NavController,
+					public navParams: NavParams,
+					public geolocation: Geolocation,
+					public globalProvider: GlobalProvider,
+					public searchProvider: SearchProvider,
+					public loadingCtrl: LoadingController,
+					public storage: Storage,) {
+
+
+		this.storage.get("logged").then((logged) => {
+			if(logged) {
+				this.logged = logged;
+				this.onReceiveUsers();
+			} else {
+				this.globalProvider.onLogout();
+				this.navCtrl.setRoot(LoginPage);
+			}
+		});
+	}
+
+	onReceiveUsers() {
+		this.loading = this.loadingCtrl.create({
+			content: 'Recebendo Dados, Aguarde ...'
+		});
+
+		this.loading.present();
+
+		this.searchProvider.onSearchUserGps(this.page, this.logged.gps_latitude, this.logged.gps_longitude).subscribe(
+			success=>this.onReceiveSuccess(success),
+			error=>this.onReceiveError(error)
+		);
+
+		this.page += 1;
+	}
+
+	onReceiveSuccess(success) {
+		console.log(success);
+		this.loading.dismiss();
+	}
+
+	onReceiveError(error) {
+		console.log(error);
+		this.loading.dismiss();
 	}
 
 	ionViewDidLoad() {
